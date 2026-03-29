42a70174060a25ce0a260d560bb43053078ceed1
Howard Lin <haward79@mail.haward79.tw>
2026-03-30 06:41:53 +0800

    feat/dockerize dockerize the app [add dockerfile, compose file, and cron helper script]

---
+++ cron_helper.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$(realpath "$0")")" || {
+    echo 'FATAL: Failed to change working dir to project root'
+    exit 1
+}
+
+[[ -z "$1" ]] && {
+    echo 'FATAL: please specify python script name to run'
+    exit 2
+}
+
+py_script="${1}.py"
+
+[[ -f "$py_script" ]] || {
+    echo "FATAL: the python script $py_script is NOT a regular file"
+    exit 3
+}
+
+docker compose exec app python "$py_script"
