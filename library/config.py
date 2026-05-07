f5fc15152f1c21b5b4cabe2896e6573022f9050d
Howard Lin <haward79@mail.haward79.tw>
2026-05-08 01:34:16 +0800

    fix sonarqube complains

--- library/config.py
+++ library/config.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, List
 from caldav import DAVClient
 import yaml
 
@@ -8,6 +8,16 @@ import yaml
 def quit_on_fatal():
     print('FATAL: fix your config first, program exit')
     exit(1)
+
+
+def to_list(value: Any) -> List[Any]:
+    if value is None:
+        return []
+
+    if not isinstance(value, list):
+        return [value,]
+
+    return value
 
 
 def read_config(
@@ -26,13 +36,10 @@ def read_config(
         configs_raw = yaml.safe_load(fin) or {}
 
     for config_key in template_dict:
-        if isinstance(template_dict[config_key], list):
-            config_raw = configs_raw.get(config_key)
+        config_raw = configs_raw.get(config_key)
 
-            if config_raw is None:
-                config_raw = []
-            elif not isinstance(config_raw, list):
-                config_raw = [config_raw,]
+        if isinstance(template_dict[config_key], list):
+            config_raw = to_list(config_raw)
 
             if not isinstance(configs[config_key], list):
                 configs[config_key] = []
@@ -40,7 +47,7 @@ def read_config(
             configs[config_key].extend(config_raw)
 
         else:
-            configs[config_key] = configs_raw.get(config_key)
+            configs[config_key] = config_raw
 
         # Check if current config_key got valid value
         if (
